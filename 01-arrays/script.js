6da21ab8dbbc5d167d452fadd036be056c5053c3
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-02 18:33:48 +0200

    fix-arrays-10

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -55,5 +55,10 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.length;
-console.log(isTeachersEmpty);
+if (teachers.length != 0){
+  const isTeachersEmpty = "not_empty";
+  console.log(isTeachersEmpty);
+} else {
+  const isTeachersEmpty = "empty";
+  console.log(isTeachersEmpty);
+}

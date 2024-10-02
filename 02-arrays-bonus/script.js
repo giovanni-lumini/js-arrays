ee40d0d5f74d59d62671583e1a963f67e8b65e98
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-02 16:41:56 +0200

    arrays-bonus-2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,9 +16,9 @@ const teachers = [
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(teacher => teacher.length >= 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers

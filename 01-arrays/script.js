9a76aec7cce5c72f7d20625a52282c3515101ccc
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-02 15:55:03 +0200

    arrays-4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,16 +22,14 @@ console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-
 const lastTeacher=teachers.pop();
 console.log(lastTeacher);
 console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-/* const firstTeacher=teachers.shift();
+const firstTeacher=teachers.shift();
 console.log(firstTeacher);
-teachers.shift();
-console.log(teachers); */
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 

40dcea56a0cfd79be26ca7b02939d5176e853663
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-02 15:42:33 +0200

    arrays-3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -17,12 +17,15 @@ const fourthTeacher = teachers.slice(3,4);
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4,1,"Patrick")
+teachers.splice(4,1,"Patrick");
 console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+lastTeacher=teachers.pop();
+console.log(lastTeacher);
+teachers.pop();
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
